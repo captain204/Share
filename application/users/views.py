05c501501c0e1a52a3ed4e0f19bb1c7f86b694ba
captain204 <nurudeenakindele8@gmail.com>
2020-03-07 16:49:12 +0100

    forms

--- application/users/views.py
+++ application/users/views.py
@@ -2,3 +2,10 @@ from flask import Blueprint
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Length
+
+users = Blueprint('users', __name__, template_folder='templates')
+
+classLoginForm(Form):
+
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(),Length(min=6)])
